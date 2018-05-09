838f83b571391826898b415bc552ddd7a5e5ac5c
Anders Hansson <anders.pm.hansson@gmail.com>
2018-05-09 19:32:34 +0200

    Corrected lapsus.

--- Freeform.h
+++ Freeform.h
@@ -30,9 +30,9 @@
 #define MIPS_OP_MTHI    0x11
 #define MIPS_OP_MFLO    0x12
 #define MIPS_OP_MTLO    0x13
-#define MIPS_OP_SB	    0x28
-#define MIPS_OP_SH	    0x29
-#define MIPS_OP_SW	    0x2B
+#define MIPS_OP_SB      0x28
+#define MIPS_OP_SH      0x29
+#define MIPS_OP_SW      0x2B
 #define MIPS_OP_ORI     0x0D
 #define MIPS_OP_XOR     0x26
 #define MIPS_OP_SRL     0x03
@@ -41,12 +41,12 @@
 #define MIPS_COP0_MF    0x00
 #define MIPS_COP0_MT    0x04
 
-#define MIPS_R_INST(opcode, rs, rt, rd, shamt, funct)	\
-    (((opcode) << 26) | ((rs) << 21) | ((rt) << 16) |	\
-    ((rd) << 11)| ((shamt) << 6) | (funct))
+#define MIPS_R_INST(opcode, rs, rt, rd, shamt, funct)   \
+  (((opcode) << 26) | ((rs) << 21) | ((rt) << 16) |     \
+  ((rd) << 11)| ((shamt) << 6) | (funct))
 
-#define MIPS_I_INST(opcode, rs, rt, immd)	\
-    (((opcode) << 26) | ((rs) << 21) | ((rt) << 16) | (immd))
+#define MIPS_I_INST(opcode, rs, rt, immd) \
+  (((opcode) << 26) | ((rs) << 21) | ((rt) << 16) | (immd))
 
 #define MIPS_J_INST(opcode, addr) (((opcode) << 26) | (addr))
 
@@ -96,66 +96,66 @@
 
 #pragma mark - Coprocessor 0 Defines
 
-#define CP0_INDEX		0
-#define CP0_RANDOM		1
-#define CP0_ENTRYLO0	2
-#define CP0_ENTRYLO1	3
-#define CP0_CONTEXT		4
-#define CP0_PAGEMASK	5
-#define CP0_WIRED		6
-#define CP0_7			7
-#define CP0_BADVADDR	8
-#define CP0_COUNT		9
-#define CP0_ENTRYHI		10
-#define CP0_COMPARE		11
-#define CP0_STATUS		12
-#define CP0_CAUSE		13
-#define CP0_EPC			14
-#define CP0_PRID		15
-#define CP0_CONFIG		16
-#define CP0_LLADDR		17
-#define CP0_WATCHLO		18
-#define CP0_WATCHHI		19
-#define CP0_20			20
-#define CP0_21			21
-#define CP0_22			22
-#define CP0_DEBUG		23
-#define CP0_DEPC		24
-#define CP0_PERFCNT		25
-#define CP0_ERRCTL		26
-#define CP0_CACHEERR	27
-#define CP0_TAGLO		28
-#define CP0_TAGHI		29
-#define CP0_ERROREPC	30
-#define CP0_DESAVE		31
+#define CP0_INDEX       0
+#define CP0_RANDOM      1
+#define CP0_ENTRYLO0    2
+#define CP0_ENTRYLO1    3
+#define CP0_CONTEXT     4
+#define CP0_PAGEMASK    5
+#define CP0_WIRED       6
+#define CP0_7           7
+#define CP0_BADVADDR    8
+#define CP0_COUNT       9
+#define CP0_ENTRYHI    10
+#define CP0_COMPARE    11
+#define CP0_STATUS     12
+#define CP0_CAUSE      13
+#define CP0_EPC        14
+#define CP0_PRID       15
+#define CP0_CONFIG     16
+#define CP0_LLADDR     17
+#define CP0_WATCHLO    18
+#define CP0_WATCHHI    19
+#define CP0_20         20
+#define CP0_21         21
+#define CP0_22         22
+#define CP0_DEBUG      23
+#define CP0_DEPC       24
+#define CP0_PERFCNT    25
+#define CP0_ERRCTL     26
+#define CP0_CACHEERR   27
+#define CP0_TAGLO      28
+#define CP0_TAGHI      29
+#define CP0_ERROREPC   30
+#define CP0_DESAVE     31
 
 #pragma mark - Status Register
 
-#define ST_IE		0x00000001	/* Interrupt enable */
-#define ST_EXL		0x00000002	/* Exception level */
-#define ST_ERL		0x00000004	/* Error level */
-#define ST_UM		0x00000010	/* User mode */
-#define ST_IM_SW0	0x00000100	/* Software interrupt mask 0 */
-#define ST_IM_SW1	0x00000200	/* Software interrupt mask 1 */
-#define ST_IM_IRQ0	0x00000400	/* External interrupt mask 0 */
-#define ST_IM_IRQ1	0x00000800	/* External interrupt mask 1 */
-#define ST_IM_IRQ2	0x00001000	/* External interrupt mask 2 */
-#define ST_IM_IRQ3	0x00002000	/* External interrupt mask 3 */
-#define ST_IM_IRQ4	0x00004000	/* External interrupt mask 3 */
-#define ST_IM_IRQ5	0x00008000	/* External interrupt mask 3 */
-#define ST_NMI		0x00080000	/* Reset caused by NMI */
-#define ST_TS		0x00200000	/* TLB shutdown */
-#define ST_BEV		0x00400000	/* Boot exception vectors */
+#define ST_IE       0x00000001  /* Interrupt enable */
+#define ST_EXL      0x00000002  /* Exception level */
+#define ST_ERL      0x00000004  /* Error level */
+#define ST_UM       0x00000010  /* User mode */
+#define ST_IM_SW0   0x00000100  /* Software interrupt mask 0 */
+#define ST_IM_SW1   0x00000200  /* Software interrupt mask 1 */
+#define ST_IM_IRQ0  0x00000400  /* External interrupt mask 0 */
+#define ST_IM_IRQ1  0x00000800  /* External interrupt mask 1 */
+#define ST_IM_IRQ2  0x00001000  /* External interrupt mask 2 */
+#define ST_IM_IRQ3  0x00002000  /* External interrupt mask 3 */
+#define ST_IM_IRQ4  0x00004000  /* External interrupt mask 3 */
+#define ST_IM_IRQ5  0x00008000  /* External interrupt mask 3 */
+#define ST_NMI      0x00080000  /* Reset caused by NMI */
+#define ST_TS       0x00200000  /* TLB shutdown */
+#define ST_BEV      0x00400000  /* Boot exception vectors */
 
-#define ST_CU0		0x10000000	/* Enable coprocessor 0 */
-#define ST_CU1		0x20000000	/* Enable coprocessor 1 (FPU) */
+#define ST_CU0      0x10000000  /* Enable coprocessor 0 */
+#define ST_CU1      0x20000000  /* Enable coprocessor 1 (FPU) */
 
 #pragma mark - Coprocessor 1 (FPU) Registers
 
-#define CP1_FIR		0	/* implementation and revision */
-#define CP1_FCCR	25	/* condition codes */
-#define CP1_FEXR	26	/* exceptions */
-#define CP1_FENR	28	/* enables */
-#define CP1_FCSR	31	/* control/status */
+#define CP1_FIR      0  /* implementation and revision */
+#define CP1_FCCR    25  /* condition codes */
+#define CP1_FEXR    26  /* exceptions */
+#define CP1_FENR    28  /* enables */
+#define CP1_FCSR    31  /* control/status */
 
 #endif
